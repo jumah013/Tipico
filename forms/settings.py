c30d37de73fed8193702dd11037d18e07e16d301
jumah013 <jumabenjamin17@gmail.com>
2020-05-26 21:06:07 +0300

    procfile

--- forms/settings.py
+++ forms/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 STATIC_URL = '/static/'
 MEDIA_URL='/images/'
 STATICFILES_DIRS=[
